e8cbe0325a7ebc3c102f53e637d939c894f32c26
fatheddine-bicane <fatheddine04@gmail.com>
2025-08-09 21:38:23 +0100

    feat: Makefile, headerfile, main.c

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME 		= cub3D
+FLAGS		= -Wall -Wextra -Werror -lglfw -lm -ldl -lX11 -g
+LIBMLX 		= ./libs/MLX42
+MY_LIBRARY	= ./libs/My_library
+LIBS		= $(LIBMLX)/build/libmlx42.a $(MY_LIBRARY)/my_library.a
+
+GREEN = \033[0;32m
+BLUE = \033[0;34m
+PURPLE = \033[0;35m
+YELLOW = \033[0;33m
+CYAN = \033[0;36m
+RED = \033[0;31m
+RESET = \033[0m
+
+
+SRCS = 
+
+OFILES = $(SRCS:.c=.o)
+
+all: libmlx libft $(NAME)
+
+
+libmlx:
+	@cmake $(LIBMLX) -B $(LIBMLX)/build >/dev/null && \
+	make -C $(LIBMLX)/build -j4 >/dev/null
+
+libft:
+	@make --no-print-directory -C libs/My_library
+
+$(NAME): $(LIBS) $(OFILES)
+	@$(CC) $(FLAGS) $(OFILES) $(LIBS) -o $(NAME)
+	@echo "$(GREEN)cube3D compiled successfully!$(RESET)"
+
+clean:
+	@rm -rf $(LIBMLX)/build
+	@make --no-print-directory -C $(MY_LIBRARY) fclean
+	@rm -f $(OFILES)
+	@echo "$(YELLOW)Cleaning librarys and object files$(RESET)"
+
+fclean: clean
+	@rm -rf $(NAME)
+	@echo "$(YELLOW)Removing cube3D$(RESET)"
+
+re: clean all
+
+.PHONY: all, clean, fclean, re, libmlx
---
+++ cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 13:35:24 by fbicane           #+#    #+#             */
+/*   Updated: 2025/08/09 21:31:19 by fbicane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "./libs/MLX42/include/MLX42/MLX42.h"
+# include <math.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <errno.h>
+
+#endif
