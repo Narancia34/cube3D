84875d5eb7835ac2187f5015c575e8714aeccb23
fatheddine-bicane <fatheddine04@gmail.com>
2025-08-09 22:18:40 +0100

    feat: parcing struct

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 13:35:24 by fbicane           #+#    #+#             */
-/*   Updated: 2025/08/09 21:31:19 by fbicane          ###   ########.fr       */
+/*   Updated: 2025/08/09 22:18:10 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,29 @@
 # include <stdbool.h>
 # include <sys/time.h>
 # include <errno.h>
+
+typedef struct s_parce t_parce;
+
+
+// INFO: Game main struct
+/*-----------------------------------------------*/
+typedef struct s_cub3d {
+	t_parce	*parce;
+}	t_cub3d;
+/*-----------------------------------------------*/
+
+// INFO: parcing struct
+/*-----------------------------------------------*/
+struct s_parce {
+	char		*file_name;
+	char		**map;
+	mlx_image_t	*north_texture;
+	mlx_image_t	*south_texture;
+	mlx_image_t	*east_texture;
+	mlx_image_t	*west_texture;
+	int			floor_color[3];
+	int			ceiling_color[3];
+};
+/*-----------------------------------------------*/
 
 #endif
