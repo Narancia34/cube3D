d467290f216c49310d2b3a8bf3127c3e94cec1ea
fatheddine-bicane <fatheddine04@gmail.com>
2025-09-01 10:38:00 +0100

    feat: listen for mouvment, rotation key press

--- game_mechanics/key_handler.c
+++ game_mechanics/key_handler.c
@@ -6,68 +6,75 @@
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 21:11:25 by fbicane           #+#    #+#             */
-/*   Updated: 2025/08/30 14:10:02 by fbicane          ###   ########.fr       */
+/*   Updated: 2025/09/01 10:37:10 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
+static void	player_rotation_key(t_cub3d *game, mlx_key_data_t *key)
+{
+
+	if (MLX_KEY_LEFT == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->look_left = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->look_left = false;
+	}
+	else if (MLX_KEY_RIGHT == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->look_right = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->look_right = false;
+	}
+}
+
+static void	player_mouvement_key_helper(t_cub3d *game, mlx_key_data_t *key)
+{
+	if (MLX_KEY_D == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->move_right = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->move_right = false;
+	}
+	else if (MLX_KEY_A == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->move_left = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->move_left = false;
+	}
+}
+
+static void	player_mouvement_key(t_cub3d *game, mlx_key_data_t *key)
+{
+	if (MLX_KEY_W == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->move_forward = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->move_forward = false;
+	}
+	else if (MLX_KEY_S == key->key)
+	{
+		if (MLX_PRESS == key->action)
+			game->mechanics->move_backward = true;
+		else if(MLX_RELEASE == key->action)
+			game->mechanics->move_backward = false;
+	}
+	player_mouvement_key_helper(game, key);
+}
+
 void	key_handler(mlx_key_data_t key, void *param)
 {
 	t_cub3d	*game = (t_cub3d *)param;
 
-
-	// player direction
-	if (MLX_KEY_LEFT == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->look_left = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->look_left = false;
-	}
-	else if (MLX_KEY_RIGHT == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->look_right = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->look_right = false;
-	}
-
-	// player mouvment
-	// TODO: player dosent register two keys at a time
-	// maybe change else ifs to just ifs
-	else if (MLX_KEY_W == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->move_forward = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->move_forward = false;
-	}
-	else if (MLX_KEY_S == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->move_backward = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->move_backward = false;
-	}
-	else if (MLX_KEY_D == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->move_right = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->move_right = false;
-	}
-	else if (MLX_KEY_A == key.key)
-	{
-		if (MLX_PRESS == key.action)
-			game->mechanics->move_left = true;
-		else if(MLX_RELEASE == key.action)
-			game->mechanics->move_left = false;
-	}
-
-
-	// escape key
-	else if (MLX_KEY_ESCAPE == key.key && MLX_PRESS == key.action)
+	player_rotation_key(game, &key);
+	player_mouvement_key(game, &key);
+	if (MLX_KEY_ESCAPE == key.key && MLX_PRESS == key.action)
 	{
 		// TODO: close and terminate
 		mlx_terminate(game->mlx);
