676fb935b6ba743c8418f021a90e3a7d2d126a63
fatheddine-bicane <fatheddine04@gmail.com>
2025-09-01 09:52:06 +0100

    fix: set player pov angle

--- game_mechanics/player_rotation/player_rotation.c
+++ game_mechanics/player_rotation/player_rotation.c
@@ -6,23 +6,15 @@
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 12:06:26 by fbicane           #+#    #+#             */
-/*   Updated: 2025/08/31 10:20:07 by fbicane          ###   ########.fr       */
+/*   Updated: 2025/09/01 09:46:35 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cub3d.h"
 #include <math.h>
 
-static void	set_player_direction(t_cub3d *game)
+void	set_player_direction(t_cub3d *game)
 {
-	if ('N' == game->parse->player_direction)
-		game->player_angle = -M_PI/2.0;
-	else if ('S' == game->parse->player_direction)
-		game->player_angle = M_PI / 2.0;
-	else if ('W' == game->parse->player_direction)
-		game->player_angle = M_PI;
-	else if ('E' == game->parse->player_direction)
-		game->player_angle = 0.0;
 }
 
 void	player_rotation(t_cub3d *game)
--- parsing/parse_map.c
+++ parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 09:40:13 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/08/12 17:40:47 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/09/01 09:50:50 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,13 @@ void	parse_map(t_cub3d *game)
 	find_player_position(game);
 	map_copy[game->parse->pyp][game->parse->pxp] = '0';
 	flood_fill(map_copy, game->parse->pxp, game->parse->pyp);
-	// int j = -1;
-	// while (map_copy[++j])
-	// 	printf("%s\n", map_copy[j]);
-	// printf("m here\n");
 	free_arr(map_copy);
-	// free_map_copy(map_copy, game->parse->rows);
+	if ('N' == game->parse->player_direction)
+		game->player_angle = -M_PI/2.0;
+	else if ('S' == game->parse->player_direction)
+		game->player_angle = M_PI / 2.0;
+	else if ('W' == game->parse->player_direction)
+		game->player_angle = M_PI;
+	else if ('E' == game->parse->player_direction)
+		game->player_angle = 0.0;
 }
