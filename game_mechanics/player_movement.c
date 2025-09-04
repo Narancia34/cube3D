c3ea64a955cbc09f929523e57fc145a1e973430d
fatheddine-bicane <fatheddine04@gmail.com>
2025-09-04 16:17:55 +0100

    fix: player collision in negative directions

--- game_mechanics/player_movement.c
+++ game_mechanics/player_movement.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   player_mouvment.c                                  :+:      :+:    :+:   */
+/*   player_movement.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 09:55:04 by fbicane           #+#    #+#             */
-/*   Updated: 2025/09/01 10:36:49 by fbicane          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:14:20 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,36 @@ void	player_mouvement(t_cub3d *game)
 	double	new_y = game->pyp;
 
 	player_new_position(&new_x, &new_y, game);
-	if ('1' != game->parse->map[(int)round(new_y)][(int)round(game->pxp)])
-		game->pyp = new_y;
-	if ('1' != game->parse->map[(int)round(game->pyp)][(int)round(new_x)])
-		game->pxp = new_x;
+	// if (new_y < game->pyp)
+	// {
+	// 	if ('1' != game->parse->map[(int)floor(new_y)][(int)floor(game->pxp)])
+	// 		game->pyp = new_y;
+	// }
+	// else
+	// {
+	// 	if ('1' != game->parse->map[(int)round(new_y)][(int)round(game->pxp)])
+	// 		game->pyp = new_y;
+	// }
+	//
+	// if (new_x < game->pxp)
+	// {
+	// 	if ('1' != game->parse->map[(int)floor(game->pyp)][(int)floor(new_x)])
+	// 		game->pxp = new_x;
+	// }
+	// else
+	// {
+	// 	if ('1' != game->parse->map[(int)round(game->pyp)][(int)round(new_x)])
+	// 		game->pxp = new_x;
+	// }
+
+		if ('1' != game->parse->map[(int)floor(game->pyp)][(int)floor(new_x)])
+			game->pxp = new_x;
+		if ('1' != game->parse->map[(int)floor(new_y)][(int)floor(game->pxp)])
+			game->pyp = new_y;
+
+
+	// printf(YELLOW"x pos -- %f\n", game->pxp);
+	// printf(BLUE"y pos -- %f\n", game->pyp);
 	game->mechanics->player->instances[0].y = (int32_t)(game->pyp * 5 + 2);
 	game->mechanics->player->instances[0].x = (int32_t)(game->pxp * 5 + 2);
 }
