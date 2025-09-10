a15126cb3660007136f49dce2d6c22608cfd8e34
Fatheddine <fatheddine04@gmail.com>
2025-09-10 13:23:17 +0100

    Wall collision fix (#7)
    * fix: player collision in negative directions
    
    * feat: rotate player pov using mouse
    
    * fix: change lock mous from shift to ctrl

---
+++ game_mechanics/cursor_handler.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cursor_handler.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 14:36:24 by fbicane           #+#    #+#             */
+/*   Updated: 2025/09/08 15:48:03 by fbicane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+void	cursor_handler(double xpos, double ypos, void *param)
+{
+	t_cub3d	*game;
+	double	deltax;
+	double	sensitivity;
+
+	(void)ypos;
+	game = (t_cub3d *)param;
+	if (game->mechanics->ctrl_pressed)
+		return ;
+	deltax = xpos - WIDTH / 2.0;
+	sensitivity = 0.005;
+	game->player_angle += deltax * sensitivity;
+	mlx_set_mouse_pos(game->mlx, WIDTH / 2, HEIGHT / 2);
+}
